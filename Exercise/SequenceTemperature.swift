c4dd8858dabb58684c288231d4d68f052fe9a999
90dhjeong <90dhjeong@gmail.com>
2021-03-12 23:22:06 +0900

    finish sequenceTemperature

--- Exercise/SequenceTemperature.swift
+++ Exercise/SequenceTemperature.swift
@@ -13,38 +13,34 @@ import Foundation
  매일 측정한 온도를 나타내는 N개의 정수가 빈칸을 사이에 두고 주어진다. 이 수들은 모두 -100 이상 100 이하
  */
 
-// 날짜수가 10
-// 더할 날짜가 2
-// 10개의 배열을 돌린다고 가정하고
-func getTemp(days: Int64, sumDays: Int, everyDays: [Int]) -> Int {
-    var sum: Int = 0
-    var i = 0
-    while i < sumDays {
-        sum += everyDays[i]
-        i += 1
-    } // 첫번째 2개의 값을 구함
-    
-    for dayCount in  0..<everyDays.count {
-        var temp: Int = 0
-        temp = sum - everyDays[dayCount-sumDays] + everyDays[dayCount]
-        
-        if temp > sum {
-            sum = temp
-        }
-    }
-    
-
-    return sum
-}
-
 var input = readLine()?.split(separator: " ").map( {Int(String($0))!} )
 var input2 = readLine()?.split(separator: " ").map( {Int(String($0))!} )
 
 if let a1 = input, let a2 = input2 {
-    let days = Int64(a1[0])
-    let sumDays = a1[1]
-    let everyDays = a2
-    
-    let c = getTemp(days: days, sumDays: sumDays, everyDays: everyDays)
+    let c = getTemp(n: a1[0], k: a1[1], everyDays: a2)
     print(c)
 }
+
+func getTemp(n: Int, k: Int, everyDays: [Int]) -> Int {
+    var sum: Int = 0
+    var result: Int = 0
+    
+    var def: Int = 0
+    while def < k {
+        sum += everyDays[def]
+        def += 1
+    }
+    
+    var temp: Int = sum
+    for i in k..<n {
+        temp -= everyDays[i-k]
+        temp += everyDays[i]
+        
+        if temp > sum {
+            result = temp
+        }
+        sum = temp
+    }
+    return result
+}
+
